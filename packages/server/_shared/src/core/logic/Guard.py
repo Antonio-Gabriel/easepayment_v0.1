f862e605c880ee9c088b2472a00d8a42fa5a57b3
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-02-27 18:22:25 +0100

    create bulk check in guard

--- packages/server/_shared/src/core/logic/Guard.py
+++ packages/server/_shared/src/core/logic/Guard.py
@@ -9,9 +9,20 @@ class IGuardResult:
 
 class Guard:
     @staticmethod
-    def against_null_or_empty(argument: any, argumentName: str) -> IGuardResult:
+    def against_null_or_empty(argument: any) -> IGuardResult:
         """Verify null or empty data"""
-        if argument == None and not argumentName:
+        if argument == None and not argument:
             return IGuardResult(
-                succeeded=False, message="{argumentName} is null or undefined"
+                succeeded=False, message="{argument} is null or undefined"
             )
+
+    @staticmethod
+    def against_null_or_empty_bulk(**args) -> IGuardResult:
+        """Verify multiple args if has a null or empty data"""
+
+        for index in args:
+
+            if not args[index]:
+                return IGuardResult(
+                    succeeded=False, message=f"{index} is null or undefined"
+                )
