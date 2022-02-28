329538ec15134108fbe6c7eeb0f27a255474b91c
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-02-28 11:24:40 +0100

    create enums for months

---
+++ packages/server/easepayment/src/domain/enums/Months.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+
+class Months(Enum):
+    janeiro = "Janeiro"
+    fevereiro = "Fevereiro"
+    marco = "Março"
+    abril = "Abril"
+    maio = "Maio"
+    junho = "Junho"
+    julho = "Julho"
+    agosto = "Agosto"
+    setembro = "Setembro"
+    outubro = "Outubro"
+    novembro = "Novembro"
+    dezembro = "Dezembro"
