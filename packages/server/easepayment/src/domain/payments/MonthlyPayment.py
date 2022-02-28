eb613935b5412dbe3c65d972ad50813413bc56ec
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-02-28 11:25:25 +0100

    monthly payment rule

---
+++ packages/server/easepayment/src/domain/payments/MonthlyPayment.py
@@ -0,0 +1,28 @@
+from typing import List
+from dataclasses import dataclass
+from decimal import Decimal
+
+from ..interfaces import IPayment
+
+
+@dataclass
+class MonthlyPaymentBase:
+    studentId: List[str]
+    userId: str
+    value: Decimal
+    month: str
+
+
+class MonthlyPayment(IPayment):
+    def __init__(self, month: str):
+        self.month: str = month
+
+    def payment_role(self, *args, **kwargs):
+        """Rule for payment month"""
+
+        return MonthlyPaymentBase(
+            month=self.month,
+            studentId=kwargs["studentId"],
+            userId=kwargs["userId"],
+            value=kwargs["value"],
+        )
