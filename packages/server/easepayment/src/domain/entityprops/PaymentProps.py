a127b1e89a72153240b4bbdcf271b4ddefea1319
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-02-28 11:24:29 +0100

    update monthly payment props

--- packages/server/easepayment/src/domain/entityprops/PaymentProps.py
+++ packages/server/easepayment/src/domain/entityprops/PaymentProps.py
@@ -1,11 +1,13 @@
 from dataclasses import dataclass
 from datetime import datetime
 from decimal import Decimal
+from typing import List
 
 
 @dataclass
 class PaymentProps:
-    userId: int
+    userId: str
+    studentId: List[str]
     value: Decimal = 0.00
     created_at: datetime = None
     updated_at: datetime = None
