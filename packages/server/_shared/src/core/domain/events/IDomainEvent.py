6e91cdebce5ae94ff391ad979eae38a14c72d851
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-02-28 11:23:26 +0100

    update domain event base

--- packages/server/_shared/src/core/domain/events/IDomainEvent.py
+++ packages/server/_shared/src/core/domain/events/IDomainEvent.py
@@ -1,10 +1,12 @@
 from abc import ABC, abstractmethod
 from typing import Type
 
+from packages.server.easepayment.src.domain.interfaces import IPayment
+
 
 class IDomainEvent(ABC):
     """Usecase interface"""
 
     @abstractmethod
-    def dispatch(event: Type[any]) -> int:
+    def dispatch(event: Type[any], payment_type: Type[IPayment]) -> int:
         raise NotImplementedError("Method not implemented")
