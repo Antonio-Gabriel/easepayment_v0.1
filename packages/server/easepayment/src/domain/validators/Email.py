b4982ff2c9bc6f8a64d65a8ccd24a9feacaabe13
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-03-02 00:22:34 +0100

    update email validator

--- packages/server/easepayment/src/domain/validators/Email.py
+++ packages/server/easepayment/src/domain/validators/Email.py
@@ -7,6 +7,6 @@ class Email:
         """Verify if the email address is valid"""
 
         expretion = (
-            r"/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix"
+            r"^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$"
         )
         return True if re.search(expretion, email) is not None else False
