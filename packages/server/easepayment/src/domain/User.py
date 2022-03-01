8ddefd163f820ad2170cf6bf1c676851f842dbc8
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-03-01 10:12:54 +0100

    add validators and new feactures in user entity

--- packages/server/easepayment/src/domain/User.py
+++ packages/server/easepayment/src/domain/User.py
@@ -1,6 +1,13 @@
+from dataclasses import dataclass
+from uuid import uuid4
 from packages.server._shared.src.core.domain import Entity
-
+from packages.server._shared.src.core.logic import Result
 from .entityprops import UserProps
+
+
+@dataclass
+class UserPropsResult(UserProps):
+    id: str = None
 
 
 class User:
@@ -12,6 +19,18 @@ class User:
     def create(cls, props: UserProps, id: int = None):
         """create owner object"""
 
+        if not id:
+            id = uuid4()
+
+        if not props.accountId and not props.ownerId:
+            return Result.fail("Invalid arguments, please check your arguments!")
+
         user = cls.__private(props, id)
 
-        return user
+        return Result.ok(
+            UserPropsResult(
+                id=id,
+                accountId=user.props.accountId,
+                ownerId=user.props.ownerId,
+            )
+        )
