c96193bda035404e5aab67bd51f989f24fd038cf
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-03-01 10:12:34 +0100

    add validators and new feactures in create owner entity

--- packages/server/easepayment/src/domain/Owner.py
+++ packages/server/easepayment/src/domain/Owner.py
@@ -1,6 +1,14 @@
+from dataclasses import dataclass
 from packages.server._shared.src.core.domain import Entity
+from packages.server._shared.src.core.logic import Result, Guard
 
 from .entityprops import OwnerProps
+from .validators import Email, Phone
+
+
+@dataclass
+class OwnerPropsResult(OwnerProps):
+    id: str
 
 
 class Owner:
@@ -12,6 +20,26 @@ class Owner:
     def create(cls, props: OwnerProps, id: int = None):
         """create owner object"""
 
+        guard_result = Guard.against_null_or_empty_bulk(
+            **{"name": props.name, "email": props.email, "phone": props.phone}
+        )
+
+        if guard_result is not None and not guard_result.succeeded:
+            return Result.fail(guard_result.message)
+
+        if not Email.is_valid(props.email):
+            return Result.fail("Invalid email address, pleace check your email!")
+
+        if not Phone.is_valid(props.phone):
+            return Result.fail("Invalid phone number, pleace check your phone number!")
+
         owner = cls.__private(props, id)
 
-        return owner
+        return Result.ok(
+            OwnerPropsResult(
+                id=id,
+                name=owner.props.name,
+                email=owner.props.email,
+                phone=owner.props.phone,
+            )
+        )
