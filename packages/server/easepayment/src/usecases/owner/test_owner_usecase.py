a0d768a44fb7a0eb2580ca1138c8909c883b487b
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-03-02 00:24:58 +0100

    test intrgration

---
+++ packages/server/easepayment/src/usecases/owner/test_owner_usecase.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+
+from .CreateOwner import CreateOwner
+from .OwnerRequestDTO import OwnerRequestDTO
+from ...infra.repositories import OwnerRepository
+
+
+class TestOwnerUsecase(TestCase):
+    def test_owner_usecase_integration(self):
+
+        create_owner = CreateOwner(OwnerRepository)
+        result = create_owner.execute(
+            OwnerRequestDTO(
+                name="António Gabriel",
+                email="antoniogabriel1@gmail.com",
+                phone="+244923565442",
+            )
+        )
+
+        error = result.error_value()
+
+        if error:
+            print(result.error_value())
+
+        print(result.get_value().id)
+
+        self.assertTrue(True)
