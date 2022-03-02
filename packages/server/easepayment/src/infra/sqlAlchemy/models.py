087489377a3712010bd84b57c568581531cbe834
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-03-02 12:36:24 +0100

    add new tables in models

--- packages/server/easepayment/src/infra/sqlAlchemy/models.py
+++ packages/server/easepayment/src/infra/sqlAlchemy/models.py
@@ -6,10 +6,12 @@ from sqlalchemy import (
     Column,
     Integer,
     String,
+    Text,
     MetaData,
     ForeignKey,
     Boolean,
     DateTime,
+    TIMESTAMP
 )
 
 engine = create_engine("mysql+pymysql://root:root@localhost/exercicio")
@@ -32,15 +34,31 @@ account = Table(
     Column("password", String(80), nullable=False),
 )
 
+student = Table(
+    "student",
+    metadata_object,
+    Column("id", String(40), nullable=False, primary_key=True),
+    Column("process", Integer, nullable=False),
+    Column("name", String(60), nullable=False),
+    Column("phone", String(14), nullable=False),
+    Column("email", String(60), nullable=False),
+    Column("district", String(45), nullable=False),
+    Column("location", String(45), nullable=False),
+    Column("avatar", Text, nullable=False),
+    Column("state", Boolean, default=True),
+    Column("student_id", String(40), ForeignKey("student.id")),
+)
+
 user = Table(
     "user",
     metadata_object,
     Column("id", String(40), nullable=False, primary_key=True),
     Column("account_id", String(40), ForeignKey("account.id")),
     Column("owner_id", String(40), ForeignKey("owner.id")),
+    Column("student_id", String(40), ForeignKey("student.id")),
     Column("state", Boolean, default=True),
-    Column("created_at", DateTime, default=datetime.now),
-    Column("updated_at", DateTime, default=datetime.now, onupdate=datetime.now),
+    Column("created_at", TIMESTAMP, default=datetime.now),
+    Column("updated_at", TIMESTAMP, default=datetime.now, onupdate=datetime.now),
 )
 
 
