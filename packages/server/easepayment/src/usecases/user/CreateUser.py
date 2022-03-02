5e7af8a1c228c54f5a620f90bb11d46c004f58ce
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-03-02 12:36:43 +0100

    create user usecase

---
+++ packages/server/easepayment/src/usecases/user/CreateUser.py
@@ -0,0 +1,30 @@
+from typing import Type
+from ...domain import User
+from packages.server._shared import IUseCase
+from .UserRequestDTO import UserRequestDTO
+from ...repositories import IUserRepository
+from packages.server._shared.src.core.logic import Result
+
+
+class CreateUser(IUseCase[UserRequestDTO, Result[None]]):
+    def __init__(self, user_repo: Type[IUserRepository]) -> None:
+        self.__user_repo = user_repo
+
+    def execute(self, request: UserRequestDTO) -> Result[None]:
+
+        user_result = User.create(
+            UserRequestDTO(
+                account_id=request.account_id,
+                owner_id=request.owner_id,
+                student_id=request.student_id,
+                state=True,
+            )
+        )
+
+        if user_result.error_value():
+            return Result.fail(user_result.error_value())
+
+        user_save_result = self.__user_repo.save(user_result.get_value())
+
+        if user_save_result:
+            return Result.ok(user_result.get_value())
