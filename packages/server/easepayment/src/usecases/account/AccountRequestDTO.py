b0a92ec9fd74210bb0c1ff22688cdadacda257b8
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-03-02 09:35:36 +0100

    create account repository DTO

---
+++ packages/server/easepayment/src/usecases/account/AccountRequestDTO.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass, fields
+
+
+@dataclass
+class AccountRequestDTO:
+    username: str
+    password: str
+
+    def __post_init__(self):
+        """Initialize the object"""
+
+        for field in fields(self):
+            value = getattr(self, field.name)
+            if not isinstance(value, field.type):
+                raise ValueError(
+                    f"Expected {field.name} to be of type {field.type}, got {repr(value)}"
+                )
