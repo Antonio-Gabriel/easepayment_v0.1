95c1e1dbe3fc87aec01ee112c4e7d73f6021cc18
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-03-02 09:35:27 +0100

    update account repository interface

--- packages/server/easepayment/src/repositories/IAccountRepository.py
+++ packages/server/easepayment/src/repositories/IAccountRepository.py
@@ -4,7 +4,7 @@ from ..domain.entityprops import AccountProps
 
 class IAccountRepository(ABC):
     @abstractmethod
-    def save(account: AccountProps, id: str):
+    def save(account_props: AccountProps):
         """Save account into db"""
 
         raise NotImplementedError("Method not implemented")
