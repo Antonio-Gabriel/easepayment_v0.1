674676193adbc71b2d4f76b5bcade9b3ab6ac7f5
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-03-02 12:36:33 +0100

    update user repository interface

--- packages/server/easepayment/src/repositories/IUserRepository.py
+++ packages/server/easepayment/src/repositories/IUserRepository.py
@@ -4,7 +4,7 @@ from ..domain.entityprops import UserProps
 
 class IUserRepository(ABC):
     @abstractmethod
-    def save(user: UserProps, id: str):
+    def save(user: UserProps):
         """Save user into db"""
 
         raise NotImplementedError("Method not implemented")
