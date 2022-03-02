d65d407fc459c9c62c8195d2bb7224236a6475f9
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-03-02 12:35:53 +0100

    modules

--- packages/server/easepayment/src/domain/__init__.py
+++ packages/server/easepayment/src/domain/__init__.py
@@ -4,3 +4,4 @@ from .Course import Course
 from .Payment import Payment
 from .interfaces import IPayment
 from .Owner import Owner
+from .User import User
--- packages/server/easepayment/src/infra/repositories/__init__.py
+++ packages/server/easepayment/src/infra/repositories/__init__.py
@@ -1,2 +1,3 @@
 from .OwnerRepository import OwnerRepository
 from .AccountRepository import AccountRepository
+from .UserRepository import UserRepository
