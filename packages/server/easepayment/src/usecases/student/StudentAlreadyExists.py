95a2f1decc82d9a46471dc789553ecb2b50c288e
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-03-11 18:37:30 +0100

    student already exists exception

---
+++ packages/server/easepayment/src/usecases/student/StudentAlreadyExists.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+from packages.server._shared.src.core.logic import Result, UseCaseError
+
+
+@dataclass
+class IUseCaseErrorError:
+    message: str
+
+
+class StudentAlreadyExists(Result[UseCaseError]):
+    def __init__(self, email: str) -> None:
+        super().__init__(
+            False,
+            IUseCaseErrorError(
+                message=f"The email {email} associated for this contact already exist"
+            ),
+        )
