c7d2af60ab8b67e9656c4129dcec4cbd87b969b1
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-03-11 15:58:43 +0100

    teste dybamic seed

--- server.py
+++ server.py
@@ -1,6 +1,11 @@
 #!python3
 
 import unittest
+# from packages.server.easepayment import ResolvingSeeder
 
 if __name__ == "__main__":
+
+    # seeder = ResolvingSeeder()
+    # seeder.load_entities_from_json_file(name="Classe")
+
     unittest.main()
