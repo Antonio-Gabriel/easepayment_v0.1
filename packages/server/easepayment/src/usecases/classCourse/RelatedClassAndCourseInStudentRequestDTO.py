26de6a57b07b70abb1de1af7f013aaa99b72ae5c
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-03-22 06:34:15 +0100

    add related class course DTO

---
+++ packages/server/easepayment/src/usecases/classCourse/RelatedClassAndCourseInStudentRequestDTO.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass, fields
+from decimal import Decimal
+
+
+@dataclass
+class RelatedClassAndCourseInStudentRequestDTO:
+    classId: str
+    courseId: str
+    studentId: str
+    price: Decimal = 0.00
+
+    def __post_init__(self):
+        """Initialize the object"""
+
+        for field in fields(self):
+            value = getattr(self, field.name)
+            if not isinstance(value, field.type):
+                raise ValueError(
+                    f"Expected {field.name} to be of type {field.type}, got {repr(value)}"
+                )
