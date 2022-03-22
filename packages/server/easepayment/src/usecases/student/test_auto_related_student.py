3978f0f2733623efb74e4aabdd3832fd08684b1a
Antonio-Gabriel <antoniocamposgabriel@gmail.com>
2022-03-22 21:40:13 +0100

    test auto related student

---
+++ packages/server/easepayment/src/usecases/student/test_auto_related_student.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+
+from .AutoRelatedStudent import AutoRelatedStudent
+from .AutoRelatedStudentDTO import AutoRelatedStudentDTO
+from ...infra.repositories import StudentAutoRelatedRepository
+
+
+class TestAutoRelatedStudent(TestCase):
+    def test_auto_related_student(self):
+
+        auto_related = AutoRelatedStudent(StudentAutoRelatedRepository)
+
+        result = auto_related.execute(
+            AutoRelatedStudentDTO(
+                owner_studentId="dfbe6494-a57c-49a8-82a5-ecbb94247e19",
+                studentId="aaec692c-7d68-43a2-8745-e560b7b5215b",
+            )
+        )
+
+        error = result.error_value()
+
+        if error:
+            print(result.error_value())
+        else:
+            print(result.get_value().id)
+
+        self.assertTrue(True)
