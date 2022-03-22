a39e771dd9c1b35d132bde5f2aec8e5d0e9b5220
Antonio-Gabriel <antoniocamposgabriel@gmail.com>
2022-03-22 21:39:22 +0100

    student auto related interface

---
+++ packages/server/easepayment/src/repositories/IStudentAutoRelated.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+from ..domain.entityprops import StudentProps
+
+
+class IStudentAutoRelated(ABC):
+    @abstractmethod
+    def save(owner_student_id: str, student_id: str):
+        """Save stutend related owner into db"""
+
+        raise NotImplementedError("Method not implemented")
+
+    @abstractmethod
+    def find_related_student(owner_student_id: str, student_id: str):
+        """Find related student"""
+
+        raise NotImplementedError("Method not implemented")
+
+    @abstractmethod
+    def delete(related_props: StudentProps):
+        """delete stutend related owner into db"""
+
+        raise NotImplementedError("Method not implemented")
