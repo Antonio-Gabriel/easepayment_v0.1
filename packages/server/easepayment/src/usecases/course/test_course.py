d2c412981f3d073756219cadceb2496d635e1472
Antonio-Gabriel <antoniocamposgabriel@gmail.com>
2022-03-24 19:45:01 +0100

    test course usace

--- packages/server/easepayment/src/usecases/course/test_course.py
+++ packages/server/easepayment/src/usecases/course/test_course.py
@@ -5,6 +5,10 @@ from .CourseRequestDTO import CourseRequestDTO
 from ...infra.repositories import CourseRepository
 
 from .GetCourses import GetCourses
+from .GetCourseById import GetCourseById
+
+from .UpdateCourse import UpdateCourse
+from .DeleteCourse import DeleteCourse
 
 
 class TestCourseUsecase(TestCase):
@@ -24,6 +28,37 @@ class TestCourseUsecase(TestCase):
 
         self.assertTrue(True)
 
+    def test_update_course_usecase_integration(self):
+
+        create_course = UpdateCourse(CourseRepository)
+        result = create_course.execute(
+            CourseRequestDTO(name="Ciências Econômicas", state=True),
+            id="1607f497-e980-4a86-92cd-3e1ad3978963",
+        )
+
+        error = result.error_value()
+
+        if error:
+            print(result.error_value())
+        else:
+            print(result.get_value())
+
+        self.assertTrue(True)
+
+    def test_delete_course_usecase_integration(self):
+
+        delete_course = DeleteCourse(CourseRepository)
+        result = delete_course.execute("1607f497-e980-4a86-92cd-3e1ad3978963")
+
+        error = result.error_value()
+
+        if error:
+            print(result.error_value())
+        else:
+            print(result.get_value())
+
+        self.assertTrue(True)
+
     def test_get_all_courses(self):
 
         get_all_courses = GetCourses(CourseRepository)
@@ -33,5 +68,17 @@ class TestCourseUsecase(TestCase):
         for course in result.get_value():
 
             print(course[1])
+
+        self.assertTrue(True)
+
+    def test_get_course_by_id(self):
+
+        course_by_id = GetCourseById(CourseRepository)
+
+        result = course_by_id.execute("1607f497-e980-4a86-92cd-3e1ad3978963")
+
+        for course in result.get_value():
+
+            print(course)
 
         self.assertTrue(True)
