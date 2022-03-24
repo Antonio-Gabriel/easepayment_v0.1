331eb31bf63e150bcc8c4ef3b2e2baf96500f802
Antonio-Gabriel <antoniocamposgabriel@gmail.com>
2022-03-24 11:14:31 +0100

    wallet repository interface

---
+++ packages/server/easepayment/src/repositories/IWalletRepository.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+
+class IWalletRepository(ABC):
+    @abstractmethod
+    def deposit(amount: float, user_id: str):
+        """Deposit balance in account"""
+
+        raise NotImplementedError("Method not implemented")
+
+    @abstractmethod
+    def with_draw(amount: float, user_id: str):
+        """WithDraw balance in account"""
+
+        raise NotImplementedError("Method not implemented")
+
+    @abstractmethod
+    def initialize(user_id: str):
+        """Initialize wallet"""
+
+        raise NotImplementedError("Method not implemented")
