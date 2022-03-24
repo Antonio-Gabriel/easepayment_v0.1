831c3c1169acaac851ca79ecda00be9133b555bc
Antonio-Gabriel <antoniocamposgabriel@gmail.com>
2022-03-24 11:13:31 +0100

    save class related course into db

--- packages/server/easepayment/src/infra/repositories/ClasseRelatedCourseRepository.py
+++ packages/server/easepayment/src/infra/repositories/ClasseRelatedCourseRepository.py
@@ -1,4 +1,3 @@
-from sqlalchemy.sql import select
 from ..sqlAlchemy import engine, classe_related_course
 
 from ...domain.entityprops import ClassRelatedCourseProps
@@ -8,30 +7,19 @@ from ...repositories import IClasseRelatedCourseRepository
 class ClasseRelatedCourseRepository(IClasseRelatedCourseRepository):
     def save(related_props: ClassRelatedCourseProps):
         """Related class with student"""
+
         connection = engine.connect()
         result = connection.execute(
             classe_related_course.insert(),
             {
-                "owner_id": related_props.ownerId,
-                "student_id": related_props.studentId,
+                "id": related_props.id,
+                "classe_id": related_props.classId,
+                "course_id": related_props.courseId,
+                "price": related_props.price,
             },
         )
 
         return result
-
-    def find_student_associeted(student_id: str):
-        """Find related student"""
-        connection = engine.connect()
-        query = select(classe_related_course).where(
-            classe_related_course.c.student_id == student_id,
-        )
-        result = connection.execute(query)
-
-        row = result.fetchone()
-
-        result.close()
-
-        return row
 
     def delete(related_props: ClassRelatedCourseProps):
         """delete stutend related owner into db"""
