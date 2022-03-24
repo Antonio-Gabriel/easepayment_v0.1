ebda0ba1ad80f28167b464cf31b29af25ccbb486
Antonio-Gabriel <antoniocamposgabriel@gmail.com>
2022-03-24 20:00:30 +0100

    update class related course usecase

--- packages/server/easepayment/src/usecases/classCourse/DeleteClassRelatedCourse.py
+++ packages/server/easepayment/src/usecases/classCourse/DeleteClassRelatedCourse.py
@@ -5,7 +5,7 @@ from packages.server._shared.src.core.logic import Result
 from ...repositories import IClasseRelatedCourseRepository
 
 
-class DeleteClassRelatedCourse(IUseCase[any, Result[None]]):
+class DeleteClassRelatedCourse(IUseCase[None, Result[None]]):
     def __init__(self, related_repo: Type[IClasseRelatedCourseRepository]) -> None:
         self.__related_repo = related_repo
 
