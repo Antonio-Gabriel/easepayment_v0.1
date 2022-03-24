113b52c8a3acb049cdcc94d5638dfcfd1174176d
Antonio-Gabriel <antoniocamposgabriel@gmail.com>
2022-03-24 11:12:52 +0100

    add result props base

--- packages/server/easepayment/src/domain/Wallet.py
+++ packages/server/easepayment/src/domain/Wallet.py
@@ -1,6 +1,15 @@
+from uuid import uuid4
+from datetime import datetime
+from dataclasses import dataclass
 from .entityprops import WallerProps
 
+from packages.server._shared.src.core.logic import Result
 from packages.server._shared.src.core.domain import Entity
+
+
+@dataclass
+class WallerPropsResult(WallerProps):
+    id: str = None
 
 
 class Wallet:
@@ -10,8 +19,19 @@ class Wallet:
 
     @classmethod
     def create(cls, props: WallerProps, id: int = None):
-        """create owner object"""
+        """create wallet object"""
+
+        if not id:
+            id = uuid4()
 
         wallet = cls.__private(props, id)
 
-        return wallet
+        return Result.ok(
+            WallerPropsResult(
+                id=id,
+                balance=wallet.props.balance,
+                userId=wallet.props.userId,
+                created_at=datetime.now(),
+                updated_at=datetime.now(),
+            )
+        )
