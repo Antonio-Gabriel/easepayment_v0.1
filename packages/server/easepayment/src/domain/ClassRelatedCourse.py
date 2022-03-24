8aaa8dc0821e38ca139985c158df4fae7e5e6bf1
Antonio-Gabriel <antoniocamposgabriel@gmail.com>
2022-03-24 11:12:25 +0100

    add result props base and update props

--- packages/server/easepayment/src/domain/ClassRelatedCourse.py
+++ packages/server/easepayment/src/domain/ClassRelatedCourse.py
@@ -1,18 +1,35 @@
+from uuid import uuid4
+from dataclasses import dataclass
 from .entityprops import ClassRelatedCourseProps
 
 from packages.server._shared.src.core.logic import Result
 from packages.server._shared.src.core.domain import Entity
 
 
+@dataclass
+class ClassRelatedCoursePropsResult(ClassRelatedCourseProps):
+    id: str = None
+
+
 class ClassRelatedCourse:
     class __private(Entity[ClassRelatedCourseProps]):
-        def __init__(self, props: ClassRelatedCourseProps, id: int = None):
+        def __init__(self, props: ClassRelatedCourseProps, id: str = None):
             super().__init__(props, id)
 
     @classmethod
-    def create(cls, props: ClassRelatedCourseProps, id: int = None):
+    def create(cls, props: ClassRelatedCourseProps, id: str = None):
         """Create a new ClassRelatedCourse"""
+
+        if not id:
+            id = uuid4()
 
         class_related_props = cls.__private(props, id)
 
-        return Result.ok(class_related_props)
+        return Result.ok(
+            ClassRelatedCoursePropsResult(
+                id=id,
+                classId=class_related_props.props.classId,
+                courseId=class_related_props.props.courseId,
+                price=class_related_props.props.price,
+            )
+        )
