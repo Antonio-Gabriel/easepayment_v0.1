d89853787eee88b5297ac71bf3f601ae2631c4bc
Antonio-Gabriel <antoniocamposgabriel@gmail.com>
2022-03-24 19:39:57 +0100

    delete course usecase

---
+++ packages/server/easepayment/src/usecases/course/DeleteCourse.py
@@ -0,0 +1,19 @@
+from typing import Type
+from packages.server._shared import IUseCase
+from .CourseRequestDTO import CourseRequestDTO
+from ...repositories import ICourseRepository
+from packages.server._shared.src.core.logic import Result
+
+
+class DeleteCourse(IUseCase[CourseRequestDTO, Result[None]]):
+    def __init__(self, course_repo: Type[ICourseRepository]) -> None:
+        self.__course_repo = course_repo
+
+    def execute(self, id: str) -> Result[None]:
+        """Delete course"""
+
+        course_deleted_result = self.__course_repo.delete(id)
+
+        if course_deleted_result:
+
+            return Result.ok("Deleted course")
