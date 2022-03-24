11727499100291e8e5bec3dcc10e6078711cd01f
Antonio-Gabriel <antoniocamposgabriel@gmail.com>
2022-03-24 17:25:57 +0100

    test create enrollment usecase

---
+++ packages/server/easepayment/src/usecases/enrollment/test_enrollment.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+
+from .CreateEnrollment import CreateEnrollment
+from .EnrollmentRequestDTO import EnrollmentRequestDTO
+from ...infra.repositories import EnrollmentRepository
+
+
+class TestEnrollment(TestCase):
+    def test_enrollment_usecase_integration(self):
+
+        create_enrollment = CreateEnrollment(EnrollmentRepository)
+        result = create_enrollment.execute(
+            EnrollmentRequestDTO(
+                class_related_course_id="8590a89e-bb7d-43fe-b068-471b15c37748",
+                student_id="8aec3a87-b85e-4833-bcb9-34b8e251b19f",
+            )
+        )
+
+        error = result.error_value()
+
+        if error:
+            print(result.error_value())
+
+        print(result.get_value())
+
+        self.assertTrue(True)
