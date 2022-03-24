bf0705f8b0e75538fabe2da06a09211340983a54
Antonio-Gabriel <antoniocamposgabriel@gmail.com>
2022-03-24 11:12:41 +0100

    add result props base and update props

--- packages/server/easepayment/src/domain/Course.py
+++ packages/server/easepayment/src/domain/Course.py
@@ -1,18 +1,29 @@
+from uuid import uuid4
 from datetime import datetime
+from dataclasses import dataclass
 
 from .entityprops import CourseProps
 
+from packages.server._shared.src.core.logic import Result
 from packages.server._shared.src.core.domain import Entity
+
+
+@dataclass
+class CoursePropsResult(CourseProps):
+    id: str = None
 
 
 class Course:
     class __private(Entity[CourseProps]):
-        def __init__(self, props: CourseProps, id: int = None):
+        def __init__(self, props: CourseProps, id: str = None):
             super().__init__(props, id)
 
     @classmethod
     def create(cls, props: CourseProps, id: str = None):
         """create course object"""
+
+        if not id:
+            id = uuid4()
 
         course = cls.__private(
             CourseProps(
@@ -24,4 +35,12 @@ class Course:
             id,
         )
 
-        return course
+        return Result.ok(
+            CoursePropsResult(
+                id=id,
+                name=course.props.name,
+                state=course.props.state,
+                created_at=course.props.created_at,
+                updated_at=course.props.updated_at,
+            )
+        )
