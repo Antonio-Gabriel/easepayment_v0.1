d0a2bc3e3c337cc458e87d991b39ec0e83d214d1
Antonio-Gabriel <antoniocamposgabriel@gmail.com>
2022-03-24 18:35:20 +0100

    test get all courses

--- packages/server/easepayment/src/usecases/course/test_course.py
+++ packages/server/easepayment/src/usecases/course/test_course.py
@@ -4,12 +4,16 @@ from .CreateCourse import CreateCourse
 from .CourseRequestDTO import CourseRequestDTO
 from ...infra.repositories import CourseRepository
 
+from .GetCourses import GetCourses
+
 
 class TestCourseUsecase(TestCase):
     def test_course_usecase_integration(self):
 
         create_course = CreateCourse(CourseRepository)
-        result = create_course.execute(CourseRequestDTO(name="Informática", state=True))
+        result = create_course.execute(
+            CourseRequestDTO(name="Ciências Econômicas", state=True)
+        )
 
         error = result.error_value()
 
@@ -17,5 +21,17 @@ class TestCourseUsecase(TestCase):
             print(result.error_value())
         else:
             print(result.get_value().id)
+
+        self.assertTrue(True)
+
+    def test_get_all_courses(self):
+
+        get_all_courses = GetCourses(CourseRepository)
+
+        result = get_all_courses.execute()
+
+        for course in result.get_value():
+
+            print(course[1])
 
         self.assertTrue(True)
