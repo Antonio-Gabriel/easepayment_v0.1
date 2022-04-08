6c7ad142e58eafca470b27e08c98be211d48ad4a
Antonio-Gabriel <antoniocamposgabriel@gmail.com>
2022-04-08 14:03:32 +0100

    test new usecases on student

--- packages/server/easepayment/src/usecases/student/test_student_usecase.py
+++ packages/server/easepayment/src/usecases/student/test_student_usecase.py
@@ -2,36 +2,38 @@ from unittest import TestCase
 
 from .CreateStudent import CreateStudent
 from .UpdateStudent import UpdateStudent
+from .DeleteStudent import DeleteStudent
+
 from .StudentRequestDTO import StudentRequestDTO
 from ...infra.repositories import StudentRepository
 
 
 class TestStudendUseCase(TestCase):
-    # def test_create_student_usecase(self):
+    def test_create_student_usecase(self):
 
-    #     create_student = CreateStudent(StudentRepository)
+        create_student = CreateStudent(StudentRepository)
 
-    #     result = create_student.execute(
-    #         StudentRequestDTO(
-    #             name="Kiala Gabriel",
-    #             email="kialagabriel@gmail.com",
-    #             phone="923434223",
-    #             process=2100,
-    #             avatar="",
-    #             district="Hoji-ya-henda",
-    #             location="Cazenga",
-    #             studentId="",
-    #         )
-    #     )
+        result = create_student.execute(
+            StudentRequestDTO(
+                name="Kiala Gabriel",
+                email="kialagabriel@gmail.com",
+                phone="923434223",
+                process=2100,
+                avatar="",
+                district="Hoji-ya-henda",
+                location="Cazenga",
+                studentId="",
+            )
+        )
 
-    #     error = result.error_value()
+        error = result.error_value()
 
-    #     if error:
-    #         print(result.error_value())
-    #     else:
-    #         print(result.get_value().id)
+        if error:
+            print(result.error_value())
+        else:
+            print(result.get_value().id)
 
-    #     self.assertTrue(True)
+        self.assertTrue(True)
 
     def test_update_student_usecase(self):
 
@@ -56,5 +58,20 @@ class TestStudendUseCase(TestCase):
             print(result.error_value())
         else:
             print(result.get_value().id)
+
+        self.assertTrue(True)
+
+    def test_delete_student_usecase(self):
+
+        delete_student = DeleteStudent(StudentRepository)
+
+        result = delete_student.execute(process=2100)
+
+        error = result.error_value()
+
+        if error:
+            print(result.error_value())
+        else:
+            print(result.get_value())
 
         self.assertTrue(True)
